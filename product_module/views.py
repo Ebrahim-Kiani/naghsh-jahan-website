156f5e0dde9a123d3080bf03e6c6c16c69baff93
kianiebrahim <ebimp4@gmail.com>
2023-10-02 00:32:56 +0330

    in product module, all files ready for showing products list view

--- product_module/views.py
+++ product_module/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from  django.views.generic.base import TemplateView, View
+from  django.views.generic import ListView, DetailView
+from .models import Product
 # Create your views here.
+
+class ProductListView(ListView):
+    template_name = 'product_module/product_list.html'
+    model = Product
+    context_object_name = 'products'
+    ordering = ['-price']
+    paginate_by = 6
