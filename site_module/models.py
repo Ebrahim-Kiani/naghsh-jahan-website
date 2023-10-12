0c7ed25d3f89a80bd6852e4e198b5e27812b04e6
kianiebrahim <ebimp4@gmail.com>
2023-10-12 13:12:30 +0330

    site setting module is created

---
+++ site_module/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+
+
+# Create your models here.
+
+class SiteSetting(models.Model):
+    site_name = models.CharField(max_length=200, verbose_name='نام سایت')
+    site_url = models.CharField(max_length=200, verbose_name='دامنه سایت')
+    address = models.CharField(max_length=300, verbose_name='آدرس')
+    address2 = models.CharField(max_length=300, verbose_name='آدرس2')
+    phone = models.CharField(max_length=200, null=True, blank=True, verbose_name='تلفن')
+    copy_right = models.TextField(verbose_name='متن کپی رایت سایت')
+    about_us_text = models.TextField(verbose_name='متن درباره ما سایت')
+    site_logo = models.ImageField(upload_to='images/site-setting/', verbose_name='لوگو سایت')
+    is_main_setting = models.BooleanField(max_length=200, verbose_name='تنظیمات اصلی')
+    whatsapp_link = models.CharField(max_length=200, verbose_name='لینک واتساپ')
+    instagram_link = models.CharField(max_length=200, verbose_name='لینک اینستاگرام')
+
+    class Meta:
+        verbose_name = 'تنظیمات سایت'
+        verbose_name_plural = 'تنظیمات'
+
+    def __str__(self):
+        return self.site_name
+
+
+class FooterLinkBox(models.Model):
+    title = models.CharField(max_length=200, verbose_name='عنوان')
+
+    class Meta:
+        verbose_name = 'دسته بندی لینک های فوتر'
+        verbose_name_plural = 'دسته بندی های لینک های فوتر'
+
+    def __str__(self):
+        return self.title
+
+
+class FooterLink(models.Model):
+    title = models.CharField(max_length=200, verbose_name='عنوان')
+    url = models.URLField(max_length=500, verbose_name='لینک')
+    footer_link_box = models.ForeignKey(to=FooterLinkBox, on_delete=models.CASCADE, verbose_name='دسته بندی')
+
+    class Meta:
+        verbose_name = 'لینک فوتر'
+        verbose_name_plural = 'لینک های فوتر'
+
+    def __str__(self):
+        return self.title
+
+
+class Slider(models.Model):
+    title = models.CharField(max_length=200, verbose_name='عنوان')
+    url = models.URLField(max_length=500, verbose_name='لینک')
+    url_title = models.CharField(max_length=200, verbose_name='عنوان لینک')
+    description = models.TextField(verbose_name='توضیحات اسلایدر')
+    image = models.ImageField(upload_to='images/sliders', verbose_name='تصویر اسلایدر')
+    is_active = models.BooleanField(default=True, verbose_name='فعال / غیرفعال')
+
+    class Meta:
+        verbose_name = 'اسلایدر'
+        verbose_name_plural = 'اسلایدر ها'
+
+    def __str__(self):
+        return self.title
