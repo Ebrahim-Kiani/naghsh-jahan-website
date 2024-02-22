6fa9e6513e48ae159bd8baec7ebfca866459cc11
kianiebrahim <ebimp4@gmail.com>
2024-02-22 21:34:15 +0330

    categoris list view is created in category module and ProductCategory is modifyed to be rellation to it self
    now i want modify site_header_refrences.html.

--- product_module/views.py
+++ product_module/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpRequest
 from django.shortcuts import render, redirect
-from django.views.generic.base import TemplateView, View
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView
 from .models import Product
