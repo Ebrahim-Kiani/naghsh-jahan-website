8579358bd06e467caef7180fe30a61d701d88c64
kianiebrahim <ebimp4@gmail.com>
2024-07-18 16:26:00 +0330

    pagination of procuct page is complited.
    now it's ready to make filters and search bar.

--- static/js/product.js
+++ static/js/product.js
@@ -85,3 +85,34 @@ function remove_favorite(favorite_Id){
          }
      })
 };
+
+// make paginations active
+
+     // Get the page number from the current URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const pageNumber = urlParams.get('page');
+    const pageNumberType = typeof pageNumber;
+    console.log('Page number:', pageNumber);
+    console.log(`The type of pageNumber is: ${pageNumberType}`);
+
+        // Loop through the links and perform desired actions
+
+document.addEventListener('DOMContentLoaded', function () {
+    const pageLinks = document.getElementsByClassName('page-number');
+    for (let i = 0; i < pageLinks.length; i++) {
+        if (pageNumber==null) {
+
+                    pageLinks[0].style.backgroundColor = 'red';
+                pageLinks[0].style.color = 'white';
+                };
+        if (pageLinks[i].textContent == pageNumber) {
+            pageLinks[i].style.backgroundColor = 'red';
+        pageLinks[i].style.color = 'white';
+        }else {
+            pageLinks[i].style.backgroundColor = 'white';
+        pageLinks[i].style.color = 'black';
+        }
+    }
+});
+
+
